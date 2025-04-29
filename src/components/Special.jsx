5fd222421b9eaed323a29fae09c0948f11dd5484
Said <saidazimsaidmuratov@gmail.com>
2025-04-29 22:31:53 +0500

    dsf

--- src/components/Special.jsx
+++ src/components/Special.jsx
@@ -1,5 +1,5 @@
 
-import imag from '../assets/imag.png'
+import {imag} from '../assets/imag.png'
 const Special = () => {
     const cards = [
         { id: 1, image: imag, title: 'Диваны', description: 'До 30%' },
