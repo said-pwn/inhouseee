72a463d26aff229130164e2275545f52d1645431
Said <saidazimsaidmuratov@gmail.com>
2025-04-29 22:06:57 +0500

    dsad

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+// https://vitejs.dev/config/
+
 // https://vite.dev/config/
 export default defineConfig({
   
   plugins: [
     react(),
-    tailwindcss(),
-  ],
+    tailwindcss(),],
+    build: {
+      chunkSizeWarningLimit: 1000 // лимит в KB
+    }
+  
 })
